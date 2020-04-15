4f3c3cbe664972788e49f8226bfcbc5411c88a49
Jacqui Lough <lough.jacqui@gmail.com>
2020-04-15 16:01:10 +1000

    Added a list of articles to the homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,38 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 
+export default ({ data }) => {
+  console.log(data)
 
-export default () => (
-  <Layout>
-    <h1>tiny news co</h1>
-    <p>this will eventually list the latest articles</p>
-  </Layout>
-)
+  return(
+    <Layout>
+      <h1>tiny news co</h1>
+      <p>here is a list of {data.allMarkdownRemark.totalCount} articles created in the cms</p>
+
+      <div>
+        <ul>
+        {data.allMarkdownRemark.edges.map(({ node }, index) => (
+          <li key={index}><a href={node.frontmatter.path}>{node.frontmatter.headline}</a></li>
+        ))}
+        </ul>
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: {order: DESC, fields: [frontmatter___date]}, limit: 1000) {
+      totalCount
+      edges {
+        node {
+          frontmatter {
+            path
+            headline
+          }
+        }
+      }
+    }
+  }
+`
