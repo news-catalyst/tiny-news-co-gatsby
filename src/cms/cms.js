04340fa00c1bde2378a60a267d44b340bcc163e4
Jacqui Lough <lough.jacqui@gmail.com>
2020-04-24 13:36:45 +1000

    Init CMS

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,5 +1,6 @@
 import CMS from 'netlify-cms-app'
 
 import TagSelectorWidget from "./tag-selector-widget.js"
-// import TagSelectorPreview from "./tag-selector-preview.js"
 CMS.registerWidget(`tag-selector`, TagSelectorWidget)
+
+CMS.init()
